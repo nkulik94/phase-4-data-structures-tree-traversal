72be7ae28187bc2985267c5e03e0c079c89e89b5
Naftali Kulik <nkulik1994@gmail.com>
2022-08-03 17:00:31 -0400

    commit tree 283f553239f7becdb82f00070eeb55de0970da4c

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,5 +1,29 @@
 class Tree
+  attr_accessor :root
   def initialize(root = nil)
     @root = root
+  end
+
+  def depth_traverse node, id
+    if node[:id] == id
+      right_node = node
+    else
+      puts 'hi'
+      node[:children].each { |node| depth_traverse(node, id) }
+    end
+    right_node
+  end 
+
+  def get_element_by_id id
+     visit = [self.root]
+     right_node = self.root
+     while visit.length > 0
+      node = visit.shift
+      if node[:id] == id
+        return right_node = node
+      else
+        visit = node[:children] + visit
+      end
+     end
   end
 end
